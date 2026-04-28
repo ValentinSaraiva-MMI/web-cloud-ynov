909e15f55ac31940cd49374e573eebf6a93c08d9
Valentin Saraiva <valentinsa0701@gmail.com>
2026-04-28 10:22:04 +0200

    Connexion

---
+++ firebase/auth_signin_password.js
@@ -0,0 +1,19 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import "../firebaseConfig";
+
+const auth = getAuth();
+
+export const signin = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      console.log("signin success");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+    });
+};
